f22f9f14c5d9bed195676189d87c87a26371613e
Cody Estes <cody@vantageled.com>
2018-03-07 18:20:27 +0000

    Upload New File | Android sign end batch configuration

---
+++ android-sign-end.bat
@@ -0,0 +1,25 @@
+@echo off
+:Begin
+ping -n 1 192.168.1.20
+If %errorlevel% == 1 (
+echo noreply
+GoTo :Begin
+)
+If %errorlevel% == 0 (
+GoTo :End
+)
+:End
+::Uploads Configuration to UBNT Device
+PSCP.EXE -scp -pw ubnt VAN-SE_Android_Config.cfg ubnt@192.168.1.20:/tmp/VAN-SE_Android_Config.cfg
+::Sending Configuration commands to UBNT Device
+del eth.txt
+plink -ssh -pw ubnt ubnt@192.168.1.20 "/sbin/ifconfig ath0 | grep HWaddr" > eth.txt
+plink -ssh -pw ubnt ubnt@192.168.1.20 "/sbin/ifconfig eth0 | grep HWaddr" >> eth.txt
+del 1firmware.txt
+echo Firmware Version: > 1firmware.txt
+plink -ssh -pw ubnt ubnt@192.168.1.20 "cat /etc/version" >> 1firmware.txt
+plink -ssh -pw ubnt ubnt@192.168.1.20 "cat /tmp/VAN-SE_Android_Config.cfg > /tmp/system.cfg"
+plink -ssh -pw ubnt ubnt@192.168.1.20 "cfgmtd -w -p /etc/"
+plink -ssh -pw ubnt ubnt@192.168.1.20 "reboot"
+type eth.txt
+type 1firmware.txt
