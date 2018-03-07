639d258f543dccb9c36bfb7004b181abb504911a
Cody Estes <cody@vantageled.com>
2018-03-07 18:13:51 +0000

    Upload New File | Firmware updater

---
+++ firmware-update.bat
@@ -0,0 +1,15 @@
+@echo off
+:Begin
+ping -n 1 192.168.1.20
+If %errorlevel% == 1 (
+echo noreply
+GoTo :Begin
+)
+If %errorlevel% == 0 (
+GoTo :End
+)
+:End
+echo y |PSCP.EXE -scp -pw ubnt XW-6.0.bin ubnt@192.168.1.20:/tmp
+PLINK.EXE -ssh -l ubnt -pw ubnt 192.168.1.20 "ubntbox fwupdate.real -m /tmp/XW-6.0.bin"
+echo Firmware Update Complete
+echo Please Upload Appropriate Configuration Next
