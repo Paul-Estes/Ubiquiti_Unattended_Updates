522d5bbb72cf34700c30dab04d3a610ddc211336
Cody Estes <cody@vantageled.com>
2018-03-07 18:52:44 +0000

    Updated file name of firmware

--- firmware-update.bat
+++ firmware-update.bat
@@ -9,7 +9,7 @@ If %errorlevel% == 0 (
 GoTo :End
 )
 :End
-echo y |PSCP.EXE -scp -pw ubnt XW-6.0.bin ubnt@192.168.1.20:/tmp
-PLINK.EXE -ssh -l ubnt -pw ubnt 192.168.1.20 "ubntbox fwupdate.real -m /tmp/XW-6.0.bin"
+echo y |PSCP.EXE -scp -pw ubnt XW.bin ubnt@192.168.1.20:/tmp
+PLINK.EXE -ssh -l ubnt -pw ubnt 192.168.1.20 "ubntbox fwupdate.real -m /tmp/XW.bin"
 echo Firmware Update Complete
 echo Please Upload Appropriate Configuration Next
